4cfb4100c2637f8ec21601faede479807d729141
Rifad ahmed <rifad.ja@gmail.com>
2020-09-03 12:14:17 +0600

    one tr removed

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -357,22 +357,7 @@
                                                     <td><a href="page-users-edit.html"><i class="material-icons">edit</i></a></td>
                                                     <td><a href="page-users-view.html"><i class="material-icons">remove_red_eye</i></a></td>
                                                 </tr>
-                                                <tr>
-                                                    <td></td>
-                                                    <td>306</td>
-                                                    <td><a href="page-users-view.html">fulton2009</a>
-                                                    </td>
-                                                    <td>Fulton Stafford</td>
-                                                    <td>20/09/2019</td>
-                                                    <td>Yes</td>
-                                                    <td>Location type 1</td>
-                                                    <td><span class="chip green lighten-5">
-                    <span class="green-text">Active</span>
-                  </span>
-                                                    </td>
-                                                    <td><a href="page-users-edit.html"><i class="material-icons">edit</i></a></td>
-                                                    <td><a href="page-users-view.html"><i class="material-icons">remove_red_eye</i></a></td>
-                                                </tr>
+
                                                 <tr>
                                                     <td></td>
                                                     <td>307</td>
