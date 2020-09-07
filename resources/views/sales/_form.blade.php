cfe8a7fb62697b56e2735b7c636378f7beb14276
Rifad ahmed <rifad.ja@gmail.com>
2020-09-07 16:52:48 +0600

    input field name corrected

--- resources/views/sales/_form.blade.php
+++ resources/views/sales/_form.blade.php
@@ -10,7 +10,7 @@
 </div>
 <div class="row">
     <div class="input-field col s12 m6">
-        <select class="validate" name="status" id="status" required="" >
+        <select class="validate" name="sale_status" id="status" required="" >
             <option value="" disabled selected> - Select Sale Status - </option>
             <option value="Active" @if(old('status', (isset($data ) && $data->status == 'Active'))) selected @endif>Active</option>
             <option value="Inactive" @if(old('status', (isset($data ) && $data->status == 'Inactive'))) selected @endif>Inactive</option>
