dd597f6f13c6a968be564d126749e8c760416b1c
Rifad ahmed <rifad.ja@gmail.com>
2020-09-09 12:16:44 +0600

    update operation done

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Brand;
 use Illuminate\Http\Request;
-Use App\Brand;
 class BrandController extends Controller
 {
     public function index(){
@@ -11,8 +11,9 @@ class BrandController extends Controller
         $data['brands']=Brand::all();
         return view('brands.index', $data);
     }
-    public function edit(){
+    public function edit($id){
         $data['title'] = "Edit Brand";
+        $data['data'] =Brand::findOrFail($id);
         return view('brands.edit', $data);
     }
     public function show($id){
@@ -23,5 +24,42 @@ class BrandController extends Controller
     public function create(){
         $data['title'] = "Create Brand";
         return view('brands.create', $data);
+    }
+    public function store(Request $request){
+        $request->validate([
+            'brand_name' => 'required|unique:brands',
+            'brand_status' => 'required',
+            'brand_order' => 'nullable|integer',
+
+        ]);
+        $brandModel = new Brand();
+        $brandModel->brand_name = $request->brand_name;
+        $brandModel->brand_status = $request->brand_status;
+        if($request->brand_order) {
+            $brandModel->brand_order = $request->brand_order;
+        }
+        $brandModel->save();
+
+        return redirect()->route('brand.create')->with('success','Brand has been created successfully!');
+        
+        //return view('brands.create', $data);
+    }
+    public function update(Request $request,$id){
+
+        $request->validate([
+            'brand_name' => 'required|unique:brands',
+            'brand_status' => 'required',
+            'brand_order' => 'nullable|integer',
+
+        ]);
+        $brandModel = Brand::findOrFail($id);
+        $brandModel->brand_name = $request->brand_name;
+        $brandModel->brand_status = $request->brand_status;
+        if($request->brand_order) {
+            $brandModel->brand_order = $request->brand_order;
+        }
+        $brandModel->save();
+
+        return redirect()->route('brand.edit',$id)->with('success','Brand has been Updated successfully!');
     }
 }
