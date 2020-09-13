e6ae52744c11d71c618d31f5e1ad2e536e904557
Rifad ahmed <rifad.ja@gmail.com>
2020-09-13 17:59:22 +0600

    link added to header menue

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -10,7 +10,9 @@
     <div class="row">
         <div id="card-stats" class="pt-0">
             <div class="row">
+            
                <div class="col s12 m6 l6 xl3">
+                <a href="{{route('brand.list')}}">
                   <div class="card gradient-45deg-light-blue-cyan gradient-shadow min-height-100 white-text animate fadeLeft">
                      <div class="padding-4">
                         <div class="row">
@@ -27,8 +29,12 @@
                         </div>
                      </div>
                   </div>
+                </a>
                </div>
+           
+            
                <div class="col s12 m6 l6 xl3">
+                <a href="{{route('buyer.list')}}">
                   <div class="card gradient-45deg-red-pink gradient-shadow min-height-100 white-text animate fadeLeft">
                      <div class="padding-4">
                         <div class="row">
@@ -44,42 +50,51 @@
                         </div>
                      </div>
                   </div>
+                </a>
                </div>
+           
                <div class="col s12 m6 l6 xl3">
-                  <div class="card gradient-45deg-amber-amber gradient-shadow min-height-100 white-text animate fadeRight">
-                     <div class="padding-4">
-                        <div class="row">
-                           <div class="col s7 m7">
-                              <i class="material-icons background-round mt-5">location_on
-                            </i>
-                              <p>Loactions</p>
-                           </div>
-                           <div class="col s5 m5 right-align">
-                           <h5 class="mb-0 white-text">{{$latestlocation}}</h5>
-                              <p class="no-margin">New</p>
-                              <p>{{$totallocation}}</p>
-                           </div>
+                    <a href="{{route('location.list')}}">
+                    <div class="card gradient-45deg-amber-amber gradient-shadow min-height-100 white-text animate fadeRight">
+                        <div class="padding-4">
+                            <div class="row">
+                            <div class="col s7 m7">
+                                <i class="material-icons background-round mt-5">location_on
+                                </i>
+                                <p>Loactions</p>
+                            </div>
+                            <div class="col s5 m5 right-align">
+                            <h5 class="mb-0 white-text">{{$latestlocation}}</h5>
+                                <p class="no-margin">New</p>
+                                <p>{{$totallocation}}</p>
+                            </div>
+                            </div>
                         </div>
-                     </div>
-                  </div>
+                    </div>
+                    </a>
                </div>
+
                <div class="col s12 m6 l6 xl3">
-                  <div class="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text animate fadeRight">
-                     <div class="padding-4">
-                        <div class="row">
-                           <div class="col s7 m7">
-                              <i class="material-icons background-round mt-5">airport_shuttle</i>
-                              <p>Supplier</p>
-                           </div>
-                           <div class="col s5 m5 right-align">
-                              <h5 class="mb-0 white-text">${{$totalOpeningBalance}}</h5>
-                              <p class="no-margin">Today</p>
-                           <p>{{$totalsupplier}}</p>
-                           </div>
+                <a href="{{route('supplier.list')}}">
+                    <div class="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text animate fadeRight">
+                        <div class="padding-4">
+                            <div class="row">
+                            <div class="col s7 m7">
+                                <i class="material-icons background-round mt-5">airport_shuttle</i>
+                                <p>Supplier</p>
+                            </div>
+                            <div class="col s5 m5 right-align">
+                                <h5 class="mb-0 white-text">${{$totalOpeningBalance}}</h5>
+                                <p class="no-margin">Today</p>
+                            <p>{{$totalsupplier}}</p>
+                            </div>
+                            </div>
                         </div>
-                     </div>
-                  </div>
+                    </div>
+                    </a>
                </div>
+
+
             </div>
          </div>
 
